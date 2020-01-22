2b380893a863a684621e1cf4fe42bc9ed33b85ef
Aleksandr <47306917+FailError@users.noreply.github.com>
2020-01-22 22:54:23 +0300

    parse name

--- src/_asm/parse.c
+++ src/_asm/parse.c
@@ -1,7 +1,37 @@
 #include "asm.h"
 
+char	*is_space(char *str)
+{
+	while(*str == ' ' || *str == '\t')
+	{
+		str++;
+	}
+	return (str);
+}
+
 void parse_p1(char *file, t_asm *asm_ms)
 {
-	file = 0;
-	asm_ms = 0;
+	int i;
+	char *str;
+
+	i = 0;
+	str = ft_strstr(file, "\n.name");
+	if(!str)
+		ft_printf("characters before the name\n");
+	else
+	{
+		str += 6;
+		str = is_space(str);
+		if(*str++ == '"')
+		{
+			while(str[i] != '"')
+			{
+				asm_ms->name[i] = str[i];
+				i++;
+			}
+			str += i;
+		}
+		else
+			ft_printf("characters before name\n");
+	}
 }
