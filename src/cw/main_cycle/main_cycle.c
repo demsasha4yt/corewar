462a2ac29ac74f64502e5ff94f7fd5d9a7cd10fa
Alex Dementev <demsasha4yt@yandex.ru>
2020-01-23 20:19:02 +0300

    Some changes

--- src/cw/main_cycle/main_cycle.c
+++ src/cw/main_cycle/main_cycle.c
@@ -1,20 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   clear_screen.c                                     :+:      :+:    :+:   */
+/*   main_cycle.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bharrold <bharrold@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/22 20:59:49 by bharrold          #+#    #+#             */
-/*   Updated: 2020/01/23 20:04:18 by bharrold         ###   ########.fr       */
+/*   Created: 2020/01/23 20:07:58 by bharrold          #+#    #+#             */
+/*   Updated: 2020/01/23 20:09:25 by bharrold         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include "corewar.h"
 
-void clear_screen()
+void	main_cycle(t_cw *cw)
 {
-	const char *clean_screen_ansi = "\e[1;1H\e[2J";
 	
-	write(0, clean_screen_ansi, 12);
 }
