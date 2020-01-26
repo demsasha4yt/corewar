4b01bf0c92a9652428982ffe25336ba5a6783e5c
Katerine Turnips <kturnips@ox-r4.21-school.ru>
2020-01-26 19:12:17 +0300

    added count_label function

--- src/_asm/help_functions.c
+++ src/_asm/help_functions.c
@@ -34,3 +34,19 @@ int is_comment(char *str)
 		return (1);
 	return (0);
 }
+
+int count_label(t_asm *asm_ms, char *str, int byte)
+{
+	t_label *label;
+
+	label = asm_ms->label;
+	while (label)
+	{
+		if (!(ft_strcmp(label->name, str)))
+			return (byte - label->byte);
+		label = label->next;
+	}
+	ft_printf("label not found");
+	//asm_error();
+	return (0);
+}
