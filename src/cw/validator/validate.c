a54b1faa92b1e8f582149bb08bd8cf5ac8925a4b
Corvettus <mackar.danilov@yandex.ru>
2020-01-28 18:15:34 +0300

    validate.c

--- src/cw/validator/validate.c
+++ src/cw/validator/validate.c
@@ -1,27 +1,46 @@
 #include "corewar.h"
 
-int validate(int fd, char **name, char **exec, char **cmnt)
+static int  null_check(char *b4)
+{
+	return (*b4 == 0 && *(b4 + 1) == 0 && *(b4 + 2) == 0 && *(b4 + 3) == 0 ? 0 : 1);
+}
+
+int         validate(int fd, char **name, char **exec, char **cmnt)
 {
     char    *b4;
     size_t  exec_sz;
 
-    b4 = (char*)malloc(4 + 1);
+    if ((b4 = (char*)malloc(4 + 1)) == NULL)
+    	return (1);
     b4[4] = 0;
-    ft_printf("%d\n", read(fd, b4, 4));                    //magic
-    *name = (char*)malloc(PROG_NAME_LENGTH + 1);
+	if (read(fd, b4, 4) != 4)
+		return (1);
+    if ((*name = (char*)malloc(PROG_NAME_LENGTH + 1)) == NULL)
+    	return (1);
     (*name)[PROG_NAME_LENGTH] = 0;
-    ft_printf("%d\n", read(fd, *name, PROG_NAME_LENGTH));  //name
-    ft_printf("%d\n", read(fd, b4, 4));                    //NULL
-    ft_printf("%d\n", read(fd, b4, 4));                    //exec size
+	if (read(fd, *name, PROG_NAME_LENGTH) != PROG_NAME_LENGTH)
+		return (1);
+	if (read(fd, b4, 4) != 4)
+		return (1);
+	if (null_check(b4))
+		return (1);
+	if (read(fd, b4, 4) != 4)
+		return (1);
     ft_printf("%d", ft_atoi_base(b4, 16));
     exec_sz = 0;
     exec_sz = (exec_sz = ft_atoi_base(b4, 16)) == 0 ? CHAMP_MAX_SIZE : exec_sz;
-    *cmnt = (char*)malloc(COMMENT_LENGTH + 1);
+    if ((*cmnt = (char*)malloc(COMMENT_LENGTH + 1)) == NULL)
+    	return (1);
     (*cmnt)[COMMENT_LENGTH] = 0;
-    read(fd, *cmnt, COMMENT_LENGTH);    //comment
-    read(fd, b4, 4);                    //NULL
-    *exec = (char*)malloc(exec_sz + 1);
+    if (read(fd, *cmnt, COMMENT_LENGTH) != COMMENT_LENGTH)
+        return (1);
+    if (read(fd, b4, 4) != 4)
+	    return (1);
+    if (null_check(b4))
+    	return (1);
+    if ((*exec = (char*)malloc(exec_sz + 1)) == NULL)
     (*exec)[exec_sz] = 0;
-    read(fd, *exec, exec_sz);              //exec
+    if (read(fd, *exec, exec_sz) != exec_sz)
+    	return (1);
     return (0);
 }
