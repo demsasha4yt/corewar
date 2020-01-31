61f35ac8db06b8c9064438d202ab5ef8cc738887
Alex Dementev <demsasha4yt@yandex.ru>
2020-01-31 19:55:34 +0300

    Commands

--- src/cw/main.c
+++ src/cw/main.c
@@ -6,11 +6,12 @@
 /*   By: bharrold <bharrold@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/22 16:04:22 by bharrold          #+#    #+#             */
-/*   Updated: 2020/01/31 19:19:42 by bharrold         ###   ########.fr       */
+/*   Updated: 2020/01/31 19:47:25 by bharrold         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
+#include "cw_ops.h"
 
 int main (int argc, char **argv)
 {
@@ -24,12 +25,12 @@ int main (int argc, char **argv)
 	init_visualize(&cw);
 	t_carry 	*ptr;
 	ptr = cw.carries;
-	while (ptr)
-	{
-		ft_printf("carry - %p\n", ptr);
-		cycle_read_command(ptr, &cw);
-		ptr = ptr->next;
-	}
+	// while (ptr)
+	// {
+	// 	ft_printf("carry - %p\n", ptr);
+	// 	cycle_read_command(ptr, &cw);
+	// 	ptr = ptr->next;
+	// }
 	main_cycle(&cw);
 	destroy_visualize(&cw);
 	destroy_cw(&cw);	
--- src/cw/main_cycle/main_cycle.c
+++ src/cw/main_cycle/main_cycle.c
@@ -6,11 +6,12 @@
 /*   By: bharrold <bharrold@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/23 20:07:58 by bharrold          #+#    #+#             */
-/*   Updated: 2020/01/23 23:17:41 by bharrold         ###   ########.fr       */
+/*   Updated: 2020/01/31 19:46:56 by bharrold         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
+#include "cw_ops.h"
 
 void	main_cycle(t_cw *cw)
 {
